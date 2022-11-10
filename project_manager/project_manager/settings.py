0cd2b790fc576212548e62b70010e334b1ed8f61
NikitaArd <11nik11255@gmail.com>
2022-11-10 08:55:19 +0100

    [0.0.3.0] Start new app manager_app (it is main app)

--- project_manager/project_manager/settings.py
+++ project_manager/project_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG_STATUS')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
