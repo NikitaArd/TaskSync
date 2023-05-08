11294430b83744fba5e4d46fd1d285e99812da5d
NikitaArd <11nik11255@gmail.com>
2023-05-08 14:37:58 +0200

    Fixed bug when admin already exists

--- manager_app/management/commands/default_admin.py
+++ manager_app/management/commands/default_admin.py
@@ -5,10 +5,16 @@ from django.contrib.auth.hashers import make_password
 from manager_app.models import CustomUser
 from django.conf import settings
 
+from django.db.utils import IntegrityError
+
 
 class Command(BaseCommand):
     help = "This command creates default admin user"
 
     def handle(self, *args, **options):
-        CustomUser.objects.create_superuser(email=settings.ADMIN_LOGIN, password=settings.ADMIN_PASSWORD)
+        try:
+            CustomUser.objects.create_superuser(email=settings.ADMIN_LOGIN, password=settings.ADMIN_PASSWORD)
+        except IntegrityError:
+            print('Admin is already exists')
+            return
         self.stdout.write('Admin was created')
